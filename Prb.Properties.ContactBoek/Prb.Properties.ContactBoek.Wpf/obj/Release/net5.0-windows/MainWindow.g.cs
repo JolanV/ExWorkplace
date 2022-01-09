b6eb418885981cc10f2ab2de7547c912b5bc8cf1
AngeloHoevenaar <angelo.hoevenaar@student.howest.be>
2022-01-09 14:17:00 +0100

    weird add

--- Prb.Properties.ContactBoek/Prb.Properties.ContactBoek.Wpf/obj/Release/net5.0-windows/MainWindow.g.cs
+++ Prb.Properties.ContactBoek/Prb.Properties.ContactBoek.Wpf/obj/Release/net5.0-windows/MainWindow.g.cs
@@ -149,7 +149,7 @@ namespace Prb.Properties.ContactBoek.Wpf {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Prb.Properties.ContactBoek.Wpf;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Prb.Properties.ContactBoek.Wpf;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
